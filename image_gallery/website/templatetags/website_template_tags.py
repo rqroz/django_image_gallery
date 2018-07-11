0b7a229b0447baaebea5cd94c124c408bb51ca2c
rqroz <rqroz1@gmail.com>
2018-07-11 05:12:20 -0300

    finished basic layout for user uploads

--- image_gallery/website/templatetags/website_template_tags.py
+++ image_gallery/website/templatetags/website_template_tags.py
@@ -16,3 +16,11 @@ def get_status_class(status):
         return 'status-danger'
     elif status == UploadedImage.PENDING:
         return 'status-warning'
+
+@register.filter
+def get_total_uploads(user):
+    return user.uploadedimage_set.all().count()
+
+@register.filter
+def get_status_uploads(user, status):
+    return user.uploadedimage_set.filter(status=status).count()
